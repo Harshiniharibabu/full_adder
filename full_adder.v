3b4100ba4ad1e2283ac6fcc3ec3bfac4663ee31c
Harshiniharibabu <harshini6787@gmail.com>
2025-12-03 13:58:13 +0530

    Add files via upload

---
+++ full_adder.v
@@ -0,0 +1,9 @@
+module full_adder (
+    input  wire a, b, c,   
+    output wire sum, carry   
+);
+
+    assign sum   = a ^ b ^ c;                  
+    assign carry = (a & b) | (b & c) | (a & c); 
+
+endmodule
